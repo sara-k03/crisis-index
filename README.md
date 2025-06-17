e3174bd3379be73737db38d818479170226e1769
sara-k03 <sarayu.kondaveeti@gmail.com>
2025-06-17 10:19:57 -0400

    html

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Project Name (TBD) 
 
+## Overview
 The goal of this project is to use machine learning to rank every country on a scale from 1-5 (1 = least prone, 5 = most prone) in terms of how at-risk they are in the following categories:
 
 - Armed Conflicts (civil wars, interstate wars, etc.)
 - Economic Instability (inflation, unemployment, etc.)
 - Environmental Disasters (floods, droughts, earthquakes, wildfires, etc.)
+
+## Current Functionality
+Created a map where if you hover over the country, the name displays
---
+++ map-script.js
@@ -0,0 +1,43 @@
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+fetch('world-countries.json')
+  .then(response => response.json())
+  .then(geoData => {
+    L.geoJSON(geoData, {
+      onEachFeature: function (feature, layer) {
+        const country = feature.properties.name;
+        
+        // Bind a tooltip with the country name, but don't open it immediately
+        layer.bindTooltip(country, {sticky: true});
+    
+        // Optional: style on hover
+        layer.on('mouseover', function () {
+        this.setStyle({
+            weight: 3,
+            color: '#666',
+            fillOpacity: 0.5
+        });
+        this.openTooltip();
+        });
+        layer.on('mouseout', function () {
+        this.setStyle({
+            weight: 1,
+            color: '#444',
+            fillOpacity: 0.2
+        });
+        this.closeTooltip();
+        });
+
+        // Initial style
+        layer.setStyle({
+          color: '#444',
+          weight: 1,
+          fillOpacity: 0.2
+        });
+      }
+    }).addTo(map);
+  });
